98a3477bd9411cae3d3407dfbf76efbc9c81b38f
Caio Henrique <caiohenriquedesantanadasilva@gmail.com>
2026-01-09 00:36:28 -0300

    correção de ortografia

--- exercício 3.py
+++ exercício 3.py
@@ -1,7 +1,7 @@
 #Crie um programa que leia dois números e mostre a soma entre eles.
 n1 = int(input('digite um  numero: '))
 n2 = int(input('digite uoutro numero: '))
-#usei o int antes do input para considerar cada número um numero inteiro de forma separada, assim, ao somar os numeros iram somar ao inves de juntar
+#usei o int antes do input para considerar cada número um número inteiro de forma separada, assim, ao somar os números iram somar ao invés de juntar
 s = n1 + n2
 
 print('A soma entre {} e {} é igual a {}.'.format(n1, n2, s))
